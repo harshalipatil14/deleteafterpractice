e04c0ad9193fd316ec0da8743b43c43f5949a08b
Harshali Patil <120819363+harshalipatil14@users.noreply.github.com>
2025-07-29 15:02:16 +0530

    er

--- jenkinsb25/declarative.groovy
+++ jenkinsb25/declarative.groovy
@@ -1,1 +1,31 @@
+pipeline {
+    agent { label 'webserver-prod' } // Specifies where the pipeline will run
 
+    stages {
+        stage('pull'){
+            steps{
+                echo "we are pulling the source code from git repository"
+                git 'https://github.com/harshalipatil14/deleteafterpractice.git'
+            }
+        }
+        stage('Build') {
+            steps {
+                echo "Building theis app using maven"
+                // Add build commands here
+            }
+        }
+        stage('Test') {
+            steps {
+                echo "testing this app using Sonarqube"
+                // Add test commands here
+            }
+        }
+        stage('Deploy') {
+            steps {
+                echo "deploying this app to the prod server in apache tomcat"
+                // Add deployment commands here
+            }
+        }
+    }
+}
+    
